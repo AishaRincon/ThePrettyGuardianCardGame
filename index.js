79a85a90cd8747d9e5aab6f89a51ce69a159aefe
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-06-18 18:16:43 -0700

    revised images and started to add javascript to the file. Also updated the css file.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let counter = 0
+let firstSelection = "";
+let secondSelection = "";
 
+const cards = document.querySelectorAll(".cards");
+cards.forEach((cards) => {
+    cards.addEventListener("click", () =>{
+        cards.classList.add("clicked");
+
+        if(counter === 0){
+            firstSelection = cards.getAttribute("sailor")
+            counter++;
+        } else {
+            secondSelection = cards.getAttribute("sailor")
+            counter = 0;
+        }
+    });
+});
