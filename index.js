cd012201c0ae24ba1449a2311e1504c97f678d32
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-06-19 09:08:25 -0700

    added randomize to the cards and the buttons to javascript. Still unable to get the container grid to function properly

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let counter = 0
 let firstSelection = "";
 let secondSelection = "";
 
+
 const cards = document.querySelectorAll(".cards");
 cards.forEach((cards) => {
     cards.addEventListener("click", () =>{
@@ -13,6 +14,66 @@ cards.forEach((cards) => {
         } else {
             secondSelection = cards.getAttribute("sailor")
             counter = 0;
+
+            if(firstSelection === secondSelection){
+                const correctCards = document.querySelectorAll(
+                    ".cards[sailor= '" + firstSelection + "']"
+            );
+
+//if two of the same cards are selected then it'll show as checked if the cards don't match then it remains as clicked
+                correctCards[0].classList.add("checked");
+                correctCards[0].classList.remove("clicked");
+                correctCards[1].classList.add("checked");
+                correctCards[1].classList.remove("clicked");
+//if the cards dont match then they animate and turn bacck over. Property needs to be added to both cards.
+            } else {
+                const incorrectCards = document.querySelectorAll(".cards.clicked");
+                incorrectCards[0].classList.add("shake");
+                incorrectCards[1].classList.add("shake");
+//to remove the color of the cards when the wrong selection is made, I need to create a setTimeOut function also, I have to remove the shake after 8 seconds.
+                setTimeout(() => {
+                    incorrectCards[0].classList.remove("shake");
+                    incorrectCards[0].classList.remove("clicked");
+                    incorrectCards[1].classList.remove("shake");
+                    incorrectCards[1].classList.remove("clicked");
+                }, 800);
+            }
         }
     });
 });
+
+//score
+const scoutsSavedCount = document.querySelector("span");
+const scoutsSaved = 6;
+
+scoutsSavedCount.textContent = scoutsSaved;
+
+//randomize the cards
+let 
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = 
+      [array[randomIndex], array[currentIndex]];
+      [array[randomIndex], array[currentIndex]];
+      [array[randomIndex], array[currentIndex]];
+      [array[randomIndex], array[currentIndex]];
+      [array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  var arr = [sailor];
+  shuffle(arr);
+  console.log(arr);
