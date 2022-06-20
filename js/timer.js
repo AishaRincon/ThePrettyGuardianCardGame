ff369cbc9316537ec43fa28a0d29338044ca3d62
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-06-19 19:37:39 -0700

    simplified everything and cleaned it up

--- js/timer.js
+++ js/timer.js
@@ -1,29 +1,71 @@
-const span = document.getElementById('span');
+// const span = document.getElementById("timer");
 
-export default class Timer {
-    constructor(root) {
-        root.innerHTML = Timer.getHTML();
+// export default class timer {
+//     constructor(root) {
+//         root.innerHTML = timer.getHTML();
 
-        this.element = {
-            minutes: root.querySelector(".timer__part--minutes"),
-            seconds: root.querySelector(".timer__part--seconds"),
-            control: root.querySelector(".timer__btn--control"),
-            reset: root.querySelector(".timer__btn--reset"),
-        };
-//Make sure the timer isnt running as soon as the webpage is loaded
-        this.interval = null; 
-        
-//current remaining seconds
-        this.remaining = seconds = 0;
+//         this.element = {
+//             minutes: root.querySelector(".timer__part--minutes"),
+//             seconds: root.querySelector(".timer__part--seconds"),
+//             control: root.querySelector(".timer__btn--control"),
+//             reset: root.querySelector(".timer__btn--reset"),
+//         };
+// //Make sure the timer isnt running as soon as the webpage is loaded
+//         this.interval = null; 
+
+// //current remaining seconds
+//         this.remaining = seconds = 60;
+//         this.updateInterfaceTime ();
+
+// //add an event listener for the above controlsl.
+// //start/stop button should click start
+//         this.element.control.addEventListener("click", () => {
+
+//         });
+
+//         this.element.reset.addEventListener("click", () => {
+
+//         });
+//     }
+
+//     updateInterfaceTime() {
+//         const minutes = Math.floor(this.remainingSeconds / 60);
+
+//         console.log(minutes, seconds);
+//     }}
 
 
-        this.element.control.addEventListener("click", () => {
-        });
 
-        this.element.reset.addEventListener("click", () => {
-        });
-    }
+// //Timer
+// const span = document.getElementById("timer");
 
-    updateInterfaceTime() {
+//     constructor(root);
+//         root.innerHTML = timer.getHTML();
 
-    }}
+//         this.element = {
+//             minutes: root.querySelector(".timer__part--minutes"),
+//             seconds: root.querySelector(".timer__part--seconds"),
+//             control: root.querySelector(".timer__btn--control"),
+//             reset: root.querySelector(".timer__btn--reset"),
+//         };
+// //Make sure the timer isnt running as soon as the webpage is loaded
+//         this.interval = null; 
+
+// //current remaining seconds
+//         this.remaining = seconds = 60;
+//         this.updateInterfaceTime ();
+
+// //add an event listener for the above controlsl.
+// //start/stop button should click start
+//         this.element.control.addEventListener("click", () => {
+
+//         });
+
+//         this.element.reset.addEventListener("click", () => {
+
+//         });
+
+//     updateInterfaceTime() 
+//         const minutes = Math.floor(this.remainingSeconds / 60);
+
+//         console.log(minutes, seconds);
