421de6afb35cf6bfb25bffee2c28f766a685fb30
AishaRincon <100747969+AishaRincon@users.noreply.github.com>
2022-06-20 00:05:28 -0700

    DONE

--- js/main.js
+++ js/main.js
@@ -4,21 +4,23 @@ let secondSelection = "";
 
 //select all of the cards and the individual ones
 const cards = document.querySelectorAll(".cards .card");
-cards.forEach((cards) => {
-    cards.addEventListener("click", () =>{
-        cards.classList.add("clicked");
-
+cards.forEach((card) => {
+    card.addEventListener("click", () =>{
+//when ever a card is clicked on its documented
+        card.classList.add("clicked");
+//if the counter is set to 0 then no cards have been selected. Check if the two cards are a match
         if(counter === 0){
-            firstSelection = cards.getAttribute("sailor")
+            firstSelection = card.getAttribute("sailor")
             counter++;
         } else {
-            secondSelection = cards.getAttribute("sailor")
+            secondSelection = card.getAttribute("sailor")
             counter = 0;
-
+//are they a match? 
             if(firstSelection === secondSelection){
                 const correctCards = document.querySelectorAll(
-                    ".cards[sailor='" + firstSelection + "']"
+                    ".card[sailor='" + firstSelection + "']"
             );
+//correct cards will be logged with correctCards            
 
 //if two of the same cards are selected then it'll show as checked if the cards don't match then it remains as clicked
                 correctCards[0].classList.add("checked");
@@ -27,10 +29,12 @@ cards.forEach((cards) => {
                 correctCards[1].classList.remove("clicked");
 //if the cards dont match then they animate and turn bacck over. Property needs to be added to both cards.
             } else {
-                const incorrectCards = document.querySelectorAll(".cards.clicked");
+                const incorrectCards = document.querySelectorAll(".card.clicked");
                 incorrectCards[0].classList.add("shake");
                 incorrectCards[1].classList.add("shake");
-//to remove the color of the cards when the wrong selection is made, I need to create a setTimeOut function also, I have to remove the shake after 8 seconds.
+
+//remove the color of the cards when the wrong selection is made, 
+//create a setTimeOut function and remove the shake after 8 seconds.
                 setTimeout(() => {
                     incorrectCards[0].classList.remove("shake");
                     incorrectCards[0].classList.remove("clicked");
